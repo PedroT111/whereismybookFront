e71c5cadf17212e274b290e8122c829dea39d677
braulioo94 <70220201+braulioo94@users.noreply.github.com>
2021-02-16 13:40:16 -0300

    Add files via upload

---
+++ src/proyectState.jsx
@@ -0,0 +1,75 @@
+import React,{useState,useEffect} from 'react';
+import axios from "axios";
+
+import proyectoContext from './proyectoContext'
+
+const ProyectoState =   props =>{
+
+    const [error, guardarError] = useState(false)
+    const [usuarios, guardarUsuario] =useState(
+        [{}]
+    )
+
+    ///Persona
+    const agregarUsuario = usuario => (
+        guardarUsuario( [...usuarios,  usuario])
+    )
+    
+    //Categoria
+    const [buscarCategorias, stateBuscarCategorias] = useState([{}]);
+    //State Local para activar el useEfect que envia por post a la BD
+    const [enviarCategoria, guardarEnviarCategoria] = useState(false)
+    //
+    const [mostrarLibros, guardarMostrarLibros] = useState(false)   
+
+    //Guardar el id de la categoria que se quiere eliminar o ver sus libros
+    const [categoriaID, guardarCategoriaID] = useState(0) 
+    
+    
+
+    //Conexion con la base de datos para consultar por las categorias
+    useEffect(() => {
+        const consultarApi = () => {
+            axios.get("http://localhost:3001/categoria")
+            .then(respuesta => {
+                
+                stateBuscarCategorias(respuesta.data.respuesta)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+        }
+        consultarApi(); 
+    }, [enviarCategoria]);
+    
+        
+    
+    
+
+
+    return(
+        <proyectoContext.Provider 
+            value={{
+                error,
+                usuarios,
+                buscarCategorias,
+                enviarCategoria ,
+                guardarError,
+                agregarUsuario,
+                stateBuscarCategorias,
+                guardarEnviarCategoria,
+                guardarMostrarLibros,
+                mostrarLibros,
+                guardarCategoriaID,
+                categoriaID
+                }}
+        >
+
+            {props.children}
+
+        </proyectoContext.Provider>
+    )
+}
+
+
+export default ProyectoState ;
